3e2f2f28367d231a2876b538fa38f453ffff74e7
adelina2999 <tataru.adelina99@gmail.com>
2019-11-02 19:55:41 +0200

    added operators, numbers, dot

--- src/components/Calculator/calculator.vue
+++ src/components/Calculator/calculator.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="calculator">
 		<div class="display">{{current || '0' }}</div>
-		<div class="btn">C</div>
-		<div class="btn">+/-</div>
-		<div class="btn">%</div>
-		<div class="btn operator">/</div>
-		<div class="btn">7</div>
-		<div class="btn">8</div>
-		<div class="btn">9</div>
-		<div class="btn operator">x</div>
-		<div class="btn">4</div>
-		<div class="btn">5</div>
-		<div class="btn">6</div>
-		<div class="btn operator">-</div>
-		<div class="btn">1</div>
-		<div class="btn">2</div>
-		<div class="btn">3</div>
-		<div class="btn operator">+</div>
-		<div class="btn zero">0</div>
-		<div class="btn">.</div>
+		<div @click="clear" class="btn">C</div>
+		<div @click="sign" class="btn">+/-</div>
+		<div @click="percent" class="btn">%</div>
+		<div @click="divide" class="btn operator">/</div>
+		<div @click="append ('7')" class="btn">7</div>
+		<div @click="append ('8')" class="btn">8</div>
+		<div @click="append ('9')" class="btn">9</div>
+		<div @click="times" class="btn operator">x</div>
+		<div @click="append ('4')" class="btn">4</div>
+		<div @click="append ('5')" class="btn">5</div>
+		<div @click="append ('6')" class="btn">6</div>
+		<div @click="minus" class="btn operator">-</div>
+		<div @click="append ('1')" class="btn">1</div>
+		<div @click="append ('2')" class="btn">2</div>
+		<div @click="append ('3')" class="btn">3</div>
+		<div @click="add" class="btn operator">+</div>
+		<div @click="append ('0')" class="btn zero">0</div>
+		<div @click="dot" class="btn">.</div>
 		<div class="btn operator">=</div>
   </div>
 </template>
@@ -27,8 +27,43 @@
 export default {
   data () {
     return {
-      current: ''
+			current: '',
+			operator: null, 
     }
+  },
+  methods: {
+    sign () {
+      this.current = this.current.charAt(0) === '-'
+        ? this.current.slice(1) : `-${this.current}`
+    },
+    clear () {
+      this.current = ''
+    },
+    percent () {
+      this.current = `${parseFloat(this.current) / 100}`
+		},
+		append (number) {
+			//if(!(this.current.length >= 10)) {
+				this.current += number
+			//}
+		},
+		dot () {
+			if (this.current.indexOf('.') === -1) {
+				this.append('.')
+			}
+		},
+		divide () {
+			this.operator = (a, b) => a / b
+		},
+		times () {
+			this.operator = (a, b) => a * b
+		},
+		minus () {
+			this.operator = (a, b) => a - b
+		},
+		add () {
+			this.operator = (a, b) => a + b
+		}
   }
 }
 </script>
