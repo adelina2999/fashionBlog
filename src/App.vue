acf24b8560102ea6b7a9ef443d567e8e530afca0
adelina2999 <tataru.adelina99@gmail.com>
2019-11-08 14:41:49 +0200

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <v-app>
+    <Header/>
     <Calculator/>
+    <Layout/>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
+import Header from './components/Header/header';
 import Calculator from './components/Calculator/calculator';
+import Layout from './components/Layout/layout';
+import Footer from './components/Footer/footer';
 
 export default {
   name: 'App',
 
   components: {
+    Header,
     Calculator,
+    Layout,
+    Footer,
+
   },
 
   data () {
--- src/components/Layout/layout.vue
+++ src/components/Layout/layout.vue
@@ -5,9 +5,14 @@
       Hover your mouse over me for a few seconds too see when you loaded this page
     </span>
     <span v-if="seen">Now you see me</span>
+    <div id="example">
+      <p>Original message: "{{ message }}"</p>
+      <p>Computed reversed message: "{{ reversedMessage }}"</p>
+    </div>
     <div>
       <b-button v-on:click="toogleSeen ()" variant="danger">Button</b-button>
     </div>
+    <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-parallax>
   </div>
 </template>
 
@@ -21,6 +26,15 @@ export default {
       message: 'You loaded this page on ' + new Date().toLocaleString(),
       seen: true,
       users: []
+    }
+  },
+  el: '#example',
+  data: {
+    message: 'Hello'
+  },
+  computed: {
+    reversedMessage: function () {
+      return this.message.split('').reverse().join('')
     }
   },
   created () {
